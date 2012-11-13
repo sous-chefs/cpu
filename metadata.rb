952341903f52a9ddea2b92cfe81fedd2d9ad0960
Guilhem Lettron <guilhem@lettron.fr>
2012-11-13 15:03:58 +0100

    increment version for development

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "guilhem.lettron@youscribe.com"
 license          "Apache v2.0"
 description      "Manage CPU Governor on linux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 supports         "ubuntu"
 supports         "debian"
