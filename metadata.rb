0691302250953340bc29c55c0f61765952821e92
Guilhem Lettron <guilhem@lettron.fr>
2012-11-13 14:53:54 +0100

    first working version

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "cpu"
+maintainer       "Guilhem Lettron"
+maintainer_email "guilhem.lettron@youscribe.com"
+license          "Apache v2.0"
+description      "Manage CPU Governor on linux"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1.0"
+supports         "ubuntu"
+supports         "debian"
