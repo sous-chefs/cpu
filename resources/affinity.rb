eb599e66ac87c803d576827e2e71afd42c48505c
Tim Smith <tsmith@chef.io>
2016-11-02 09:19:11 -0700

    Remove Chef 10 compatibility
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/affinity.rb
+++ resources/affinity.rb
@@ -1,12 +1,5 @@
 actions :set
-
 default_action :set
 
-attribute :pid, :kind_of => [String, Integer], :name_attribute => true
-attribute :cpu, :kind_of => [String, Integer], :required => true
-
-# Covers 0.10.8 and earlier
-def initialize(*args)
-  super
-  @action = :set
-end
+attribute :pid, kind_of: [String, Integer], name_attribute: true
+attribute :cpu, kind_of: [String, Integer], required: true
