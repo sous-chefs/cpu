77a5f8d71ce99d82615b61341b5e3ce6f834211e
Tim Smith <tsmith@chef.io>
2017-04-06 11:21:48 -0700

    Deprecate the default recipe since it’s empty
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: cpu
+# Cookbook:: cpu
 # Author:: Guilhem Lettron <guilhem.lettron@youscribe.com>
 #
-# Copyright 2012, Societe Publica.
+# Copyright:: 2012-2017, Societe Publica.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,3 +16,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+Chef::Log.warn('The cpu::default recipe has been deprecated and will be removed in a future release.')
