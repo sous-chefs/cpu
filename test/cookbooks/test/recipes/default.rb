17ddcd10085e8f81c0c7e739d60a6d17c21d8351
Tim Smith <tsmith@chef.io>
2017-04-06 19:37:19 -0700

    Test the resources on the chef-client itself
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -6,3 +6,14 @@
 apt_update 'update'
 
 include_recipe 'cpu::governor'
+
+# we need something that will be running on every system
+chef_pid = shell_out('pidof chef-client').run_command.stdout
+
+cpu_affinity chef_pid do
+  cpu 0
+end
+
+cpu_nice chef_pid do
+  priority 1
+end
