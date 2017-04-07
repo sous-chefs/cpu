775b628589d85783e209be9f797f31d73ae1d965
Tim Smith <tsmith@chef.io>
2017-04-07 15:50:48 -0700

    Release 2.0.0
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'Apache-2.0'
 description 'Manage CPU Governor on linux'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '2.0.0'
 supports 'ubuntu'
 supports 'debian'
 supports 'redhat'
